d26999ad38f503e26c71586c079515294e9f555c
Rejah Rehim <rejah@appfabs.com>
2018-08-30 14:00:48 +0530

    Implement coloured logger
    Add new file with logger class and update print calls with new logger.
    Added new banner.

--- SecureTea.py
+++ SecureTea.py
@@ -1,4 +1,12 @@
-"""Docstring.
+u"""SecureTea.
+
+Project:
+    ╔═╗┌─┐┌─┐┬ ┬┬─┐┌─┐╔╦╗┌─┐┌─┐
+    ╚═╗├┤ │  │ │├┬┘├┤  ║ ├┤ ├─┤
+    ╚═╝└─┘└─┘└─┘┴└─└─┘ ╩ └─┘┴ ┴
+
+    Version: 1.1
+    Module: SecureTea
 
 Attributes:
     ACCESS_TOKEN (str): Access token of twitter
@@ -7,7 +15,6 @@ Attributes:
     API_SECRET (str): Api secret
     auth (TYPE): Description
     debug (int): Debug flag
-    localtime (TYPE): Current time
     twitter (TYPE): Description
     twitter_username (str): Username in twitter
     welcome_msg (TYPE): Welcome message
@@ -15,24 +22,26 @@ Attributes:
 # To share mouse gestures and post on Twitter
 import time
 
-# If it is not already installed, please download & install the twitter package from
+# Please make sure to install the package twitter from
 # https://pypi.python.org/pypi/twitter
 from pynput import mouse
 from twitter import *
+import logger
 
 debug = 1
+API_KEY = 'XXXX'              # Change me
+API_SECRET = 'XXXX'           # Change me
+ACCESS_TOKEN = 'XXXX'         # Change me
+ACCESS_TOKEN_SECRET = 'XXXX'  # Change me
+twitter_username = 'XXXX'     # Change me
 
-API_KEY = 'XXXX'
-API_SECRET = 'XXXX'
-ACCESS_TOKEN = 'XXXX'
-ACCESS_TOKEN_SECRET = 'XXXX'
-twitter_username = 'XXXX'
 
-localtime = time.asctime(time.localtime(time.time()))
+moduleName = 'Notification'
+logger = logger.SecureTeaLogger(moduleName)
 auth = OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, API_KEY, API_SECRET)
 twitter = Twitter(auth=auth)
-welcome_msg = "\nWelcome to SecureTea..!! Initializing System @ " + localtime
-print(welcome_msg)
+welcome_msg = "Welcome to SecureTea..!! Initializing System"
+logger.log(welcome_msg)
 twitter.direct_messages.new(user=twitter_username, text=welcome_msg)
 alert_count = 1
 
@@ -42,7 +51,10 @@ def notification_to_twitter(msg):
     try:
         twitter.direct_messages.new(user=twitter_username, text=msg)
     except Exception as e:
-        print("Notification not sent, error is: " + str(e))
+        logger.log(
+            "Notification not sent, error is: " + str(e),
+            logtype="error"
+        )
 
 
 def on_move(x, y):
@@ -55,16 +67,14 @@ def on_move(x, y):
     global alert_count
 
     if (debug == 1):
-        print('Pointer moved to {0}'.format((x, y)))
+        logger.log('Pointer moved to {0}'.format((x, y)))
 
-    localtime = time.asctime(time.localtime(time.time()))
-
-    msg = 'Alert(' + str(alert_count) + \
-        ') : Someone has access your laptop when ' + localtime
+    msg = '(' + str(alert_count) + \
+        ') : Someone has access your laptop when '
 
     # Shows the warning msg on the console
     if (debug == 1):
-        print(msg)
+        logger.log(msg, logtype="warning")
 
     # Send a warning message via twitter account
     notification_to_twitter(msg)
@@ -73,13 +83,13 @@ def on_move(x, y):
     alert_count += 1
 
     if (debug == 1):
-        print("The program will sleep for 10 seconds")
+        logger.log("The program will sleep for 10 seconds")
 
     time.sleep(10)
 
     # Ready to monitor the next move
     if (debug == 1):
-        print("Ready to monitor further movement .. !!")
+        logger.log("Ready to monitor further movement .. !!")
 
     # Stop the listener
     return False
@@ -93,9 +103,21 @@ def main():
             with mouse.Listener(on_move=on_move) as listener:
                 listener.join()
     except Exception as e:
-        print("Something went wrong: " + str(e))
+        logger.log(
+            "Something went wrong: " + str(e),
+            logtype="error"
+        )
 
 
 if __name__ == '__main__':
-    main()
-    print("End of program")
+
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        logger.log(
+            "You pressed Ctrl+C!, Bye")
+    except:
+        logger.log(
+            "Something went wrong: " + str(e) + "End of program",
+            logtype="error"
+        )
