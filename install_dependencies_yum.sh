50f8c801f0b2761ee2e3946999d9a68db28791f7
MajAK <majmundarkushal9@gmail.com>
2020-07-28 10:14:48 +0530

    Update install_dependencies_yum.sh

--- install_dependencies_yum.sh
+++ install_dependencies_yum.sh
@@ -12,8 +12,9 @@
 
 yum check-update
 yum groupinstall -y "Development Tools" "Development Libraries"
-yum install -y python3-setuptools python3-devel libnetfilter_queue libnfnetlink-devel libnetfilter_queue-devel rsyslog
 yum install -y epel-release
+yum install -y python3-setuptools python3-devel libnetfilter_queue libnfnetlink-devel libnetfilter_queue-devel rsyslog
+service rsyslog restart
 yum install -y clamav clamav-update
 yum install -y clamav-server clamav-data clamav-update clamav-filesystem clamav clamav-scanner-systemd clamav-devel clamav-lib clamav-server-systemd
 python3 -m pip install -r requirements.txt
