f2510dd533dfd2bc0ff46011d9461c81efb96a0b
pwned-17 <shaikajmal.r2000@hotmail.com>
2021-06-17 18:19:50 +0530

    Update

--- securetea/lib/waf/proxy/proxy.py
+++ securetea/lib/waf/proxy/proxy.py
@@ -16,25 +16,24 @@ from intercept import Http
 
 class RunProxy:
     """
+     A class that starts the proxy server
 
 
     """
     def __init__(self):
         """
-
+         Initialize host and port for listening
         """
         self.host="127.0.0.1"
         self.port=2345
 
 
     def run_server(self):
-        """ """
+
         asyncio.run(self.start())
 
     async def start(self):
-        """
 
-        """
         self.loop=asyncio.get_event_loop()
         self.server = await self.loop.create_server(
             lambda:Http(),host=self.host, port=self.port
