966ef5a92a0be27f5a800238365bfc310cfe7b4e
Digvijay Bhosale <70275323+DigvijayBhosale1729@users.noreply.github.com>
2021-08-11 18:51:03 +0530

    Removes redundant statement during installation
    python3 -m pip install -r requirements.txt needs to be repeated during installation, but including it again here gives cleaner installation

--- install_dependencies_apt.sh
+++ install_dependencies_apt.sh
@@ -18,7 +18,7 @@ apt-get install software-properties-common
 add-apt-repository ppa:deadsnakes/ppa
 apt-get update
 apt-get install python3.8
-apt-get install python3-pip
+apt-get install python3-pip # installing for clean/new/fresh distributions
 python3 -m pip install -r requirements.txt
 python3 -m pip install git+https://github.com/kti/python-netfilterqueue
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt # removes need for extra statement in installation procedure
