26ca3e1abee3ca0c56e485dfc6acfb050ef63b51
Digvijay Bhosale <70275323+DigvijayBhosale1729@users.noreply.github.com>
2021-08-11 17:34:57 +0530

    Added Python and pip installation for blank/new OS
    A new/blank OS might not come with python3/pip3/git preinstalled, hence added statements to install those for smoother progress.

--- install_dependencies_apt.sh
+++ install_dependencies_apt.sh
@@ -14,5 +14,10 @@ apt-get update
 apt install -y python3-setuptools build-essential python3-dev libnfnetlink-dev libnetfilter-queue-dev libnetfilter-queue1 rsyslog
 service rsyslog restart
 apt-get install -y clamav
+apt-get install software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+apt-get install python3.8
+apt-get install python3-pip
 python3 -m pip install -r requirements.txt
 python3 -m pip install git+https://github.com/kti/python-netfilterqueue
