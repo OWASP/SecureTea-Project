8dc9f01e5269b807143969a67c57ae50b8e0461a
Digvijay Bhosale <70275323+DigvijayBhosale1729@users.noreply.github.com>
2021-08-12 10:43:31 +0530

    Testing

--- install_dependencies_apt.sh
+++ install_dependencies_apt.sh
@@ -19,6 +19,6 @@ add-apt-repository ppa:deadsnakes/ppa
 apt-get update
 apt-get install python3.8
 apt-get install python3-pip # installing for clean/new/fresh distributions
-python3 -m pip install -r requirements.txt
 python3 -m pip install git+https://github.com/kti/python-netfilterqueue
+python3 -m pip install -r requirements.txt
 python3 -m pip install -r requirements.txt # removes need for extra statement in installation procedure
