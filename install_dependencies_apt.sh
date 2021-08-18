a7bfb95f4b205543096783376806b776e0d52734
Digvijay Bhosale <70275323+DigvijayBhosale1729@users.noreply.github.com>
2021-08-18 16:26:45 +0530

    Update install_dependencies_apt.sh

--- install_dependencies_apt.sh
+++ install_dependencies_apt.sh
@@ -10,23 +10,23 @@
 #     distros : APT package manager based
 #     files_definition (TYPE): Dependencies installation script
 
-apt-get update
+apt-get -y -qq update
 
-apt-get install software-properties-common # installation of python3 and python3-pip
+apt-get -y -qq  install software-properties-common # installation of python3 and python3-pip
 add-apt-repository ppa:deadsnakes/ppa
-apt-get update
-apt-get install python3
-apt-get install python3-pip # python3 and pip installed
+apt-get -y -qq update
+apt-get -y -qq install python3
+apt-get -y -qq install python3-pip # python3 and pip installed
 
-apt install -y python3-setuptools build-essential python3-dev libnfnetlink-dev libnetfilter-queue-dev libnetfilter-queue1 rsyslog
+apt -y -qq install python3-setuptools build-essential python3-dev libnfnetlink-dev libnetfilter-queue-dev libnetfilter-queue1 rsyslog
 service rsyslog restart
-apt-get install -y clamav # dependencies installed
+apt-get -y -qq install -y clamav # dependencies installed
 
-apt-get install exiftool pngcheck foremost steghide stegosuite # dependencies for steg analysis
+apt-get -y -qq install exiftool pngcheck foremost steghide stegosuite # dependencies for steg analysis
 
-apt-get install curl    # dependency for API interaction.
+apt-get -y -qq install curl    # dependency for API interaction.
 
-apt-get install git # installing git
+apt-get -y -qq install git # installing git
 python3 -m pip install git+https://github.com/kti/python-netfilterqueue
 
 python3 -m pip install -r requirements.txt
