a650c554e062946e3974e0d746f81963a909c75d
shubhangi013 <choudharyshubhangi13@gmail.com>
2022-02-23 16:51:50 +0530

    improved the UI and the CLI

--- securetea/entry_points/system_ep.py
+++ securetea/entry_points/system_ep.py
@@ -41,7 +41,7 @@ def get_args():
         '--debug',
         default=False,
         action="store_true",
-        help='Degug true or false'
+        help='Debug true or false'
     )
 
     args = parser.parse_args()
