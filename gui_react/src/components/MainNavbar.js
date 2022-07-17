ef83da83453640776b22922c7f3cd75ced860395
DigvijayBhosale1729 <darkamethyst1729@gmail.com>
2022-07-17 18:09:28 +0530

    react GUI

--- gui_react/src/components/MainNavbar.js
+++ gui_react/src/components/MainNavbar.js
@@ -14,12 +14,12 @@ export default function MainNavbar(){
 
                 {/* <!-- Brand --> */}
                 <a class="navbar-brand" href="#test">
-                <img src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp" height="25" alt="MDB Logo"
+                <img src="https://owasp.org/assets/images/logo.png" height="50" alt="OWASP Logo"
                     loading="lazy" />
                 </a>
                 {/* <!-- Search form --> */}
                 <form class="d-none d-md-flex input-group w-auto my-auto">
-                <input autocomplete="off" type="search" class="form-control rounded"
+                <input autocomplete="on" type="search" class="form-control rounded"
                     placeholder='Search (ctrl + "/" to focus)' style={{ 'min-width':`225px` }} />
                 <span class="input-group-text border-0"><i class="fas fa-search"></i></span>
                 </form>
@@ -30,19 +30,13 @@ export default function MainNavbar(){
                 <li class="nav-item dropdown">
                     <a class="nav-link me-3 me-lg-0 dropdown-toggle hidden-arrow" href="#test" id="navbarDropdownMenuLink"
                     role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-                    <i class="fas fa-bell"></i>
-                    <span class="badge rounded-pill badge-notification bg-danger">1</span>
+                        <i class="fas fa-bell"></i>
+                        <span class="badge rounded-pill badge-notification bg-danger">1</span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
-                    <li>
-                    <a class="dropdown-item" href="#test">Some news</a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="#test">Another news</a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="#test">Something else here</a>
-                    </li>
+                        <li><a class="dropdown-item" href="#test">Some news</a></li>
+                        <li><a class="dropdown-item" href="#test">Another news</a></li>
+                        <li><a class="dropdown-item" href="#test">Something else here</a></li>
                     </ul>
                 </li>
 
@@ -59,54 +53,11 @@ export default function MainNavbar(){
                     </a>
                 </li>
 
-                {/* <!-- Icon dropdown --> */}
-                <li class="nav-item dropdown">
-
-                    <a class="nav-link me-3 me-lg-0 dropdown-toggle hidden-arrow" href="#test" id="navbarDropdown"
-                    role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-                    <i class="united kingdom flag m-0"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li>
-                    <a class="dropdown-item" href="#test"><i class="united kingdom flag"></i>English
-                    <i class="fa fa-check text-success ms-2"></i></a>
-                    </li>
-                    <li>
-                    <hr class="dropdown-divider" />
-
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="#test"><i class="flag-poland flag"></i>Polski</a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="#test"><i class="flag-china flag"></i>中文</a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="#test"><i class="flag-japan flag"></i>日本語</a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="#test"><i class="flag-germany flag"></i>Deutsch</a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="#test"><i class="flag-france flag"></i>Français</a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="#test"><i class="flag-spain flag"></i>Español</a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="#test"><i class="flag-russia flag"></i>Русский</a>
-                    </li>
-                    <li>
-                    <a class="dropdown-item" href="#test"><i class="flag-portugal flag"></i>Português</a>
-                    </li>
-                    </ul>
-                </li>
-
                 {/*<!-- Avatar --> */}
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle hidden-arrow d-flex align-items-center" href="#test"
                     id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp" class="rounded-circle"
+                    <img src="https://owasp.org/assets/images/logo.png" class="rounded-circle"
                     height="22" alt="Avatar" loading="lazy" />
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
--- gui_react/src/components/SideNavbar.js
+++ gui_react/src/components/SideNavbar.js
@@ -7,30 +7,20 @@ export default function SideNavbar(){
               <div class="position-sticky">
                 <div class="list-group list-group-flush mx-3 mt-4">
                   <a href="#test" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
-                    <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Main dashboard</span>
+                    <span>Main dashboard</span>
                   </a>
-                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple active">
-                    <i class="fas fa-chart-area fa-fw me-3"></i><span>Webiste traffic</span>
-                  </a>
-                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple"><i
-                      class="fas fa-lock fa-fw me-3"></i><span>Password</span></a>
-                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple"><i
-                      class="fas fa-chart-line fa-fw me-3"></i><span>Analytics</span></a>
                   <a href="#test" class="list-group-item list-group-item-action py-2 ripple">
-                    <i class="fas fa-chart-pie fa-fw me-3"></i><span>SEO</span>
-                  </a>
-                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple"><i
-                      class="fas fa-chart-bar fa-fw me-3"></i><span>Orders</span></a>
-                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple"><i
-                      class="fas fa-globe fa-fw me-3"></i><span>International</span></a>
-                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple"><i
-                      class="fas fa-building fa-fw me-3"></i><span>Partners</span></a>
-                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple"><i
-                      class="fas fa-calendar fa-fw me-3"></i><span>Calendar</span></a>
-                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple"><i
-                      class="fas fa-users fa-fw me-3"></i><span>Users</span></a>
-                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple"><i
-                      class="fas fa-money-bill fa-fw me-3"></i><span>Sales</span></a>
+                    <span>Storage</span></a>
+                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple">
+                    <span>Processes</span></a>
+                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple">
+                    <span>Last Login</span></a>
+                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple">
+                    <span>Network</span></a>
+                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple">
+                    <span>Secrity</span></a>
+                  <a href="#test" class="list-group-item list-group-item-action py-2 ripple">
+                    <span>Logout</span></a>
                 </div>
               </div>
             </nav>
